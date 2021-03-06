cd52d7d278333617edd81923faeee5a2de8ce2e2
NicolaDean <nicola.dean@mail.polimi.it>
2021-03-06 14:50:06 +0100

    Cancello Funzioni TEST

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,24 +1,9 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
     public static void main(String[] args) {
 
-        int b = 1;
-        int pippo = 42;
         System.out.println("Hello AM12 GROUP!");
-    }
-
-    public int funzionefede()
-    {
-        return 1;
-    }
-
-    public int funzioneRichi()
-    {
-        return 2;
     }
 }
