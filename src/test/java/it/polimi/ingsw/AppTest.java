a648cdbe8dcd131d33cbd0de4285142783a96e27
fde99 <federicofilippo.diana@mail.polimi.it>
2021-03-20 17:20:42 +0100

    Added the class "Card"

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,8 +1,9 @@
 package it.polimi.ingsw;
 
 
-import static org.junit.jupiter.api.Assertions.*;
-import  org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
