16c33c29dbd342ad7df3995432b16b5be63b820c
NicolaDean <nicola.dean@mail.polimi.it>
2021-03-22 13:51:10 +0100

    Added all Method to Dashboard class: -basicProduction -getAllAvailableResource -getCardScores -setProcuctionCard -setProcuctionCard

--- src/main/java/it/polimi/ingsw/model/Dashboard.java
+++ src/main/java/it/polimi/ingsw/model/Dashboard.java
@@ -1,6 +1,19 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.enumeration.ResourceType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Stream;
+
 public class Dashboard {
+    Storage Storage;
+    Resource [] Chest;
+    Queue<ProductionCard>[] ProducionCards;
+    boolean [] PapalToken;
+
     public Dashboard()
     {
 
@@ -8,5 +21,72 @@ public class Dashboard {
 
     public int getScore(){
         return 0;
+    }
+
+    /**
+     *
+     * @param card Card to position
+     * @param pos Position where i want to put the card
+     * @return true if card correctly setted
+     */
+    public boolean setProcuctionCard(ProductionCard card,int pos)
+    {
+        return false;
+    }
+
+    /**
+     *
+     * @param card Card to position
+     * @param pos  position to check
+     * @return  true if the position is valid
+     */
+    private boolean checkValidPosition(ProductionCard card, int pos)
+    {
+        return false;
+    }
+
+    /**
+     * Excange 2 resource (of wanted type)  for 1 of a different type
+     * @param spendOne     Type i want to spend
+     * @param spendTwo     Type i want to spend
+     * @param obtain    Type i want
+     * @return true if i can do a basicProduction
+     */
+    public boolean basicProduction(ResourceType spendOne, ResourceType spendTwo,ResourceType obtain)
+    {
+        List<Resource> availableRes = this.getAllAvailableResource();
+
+        //NEED TO ADD A COMPARE OVERLOADING (list<resource>,resource)
+        List<Resource> tmp = new ArrayList<Resource>();
+        tmp.add(new Resource(spendOne,1));
+        tmp.add(new Resource(spendTwo,1));
+
+        boolean output = ResourceOperator.compare(availableRes,tmp);
+        if(output)
+        {
+            Resource ob = new Resource(obtain,1);
+            //ADD TO CHEST ob resource
+        }
+        return output;
+    }
+
+    public List<Resource> getAllAvailableResource()
+    {
+        return new ArrayList<Resource>();
+    }
+
+    /**
+     *
+     * @return the sum of all activated ProductionCards
+     */
+    public int getCardScores()
+    {
+        int vp=0;
+
+        for(Queue<ProductionCard> queue : ProducionCards)
+        {
+            vp +=queue.stream().mapToInt(ProductionCard::getScore).sum();
+        }
+        return vp;
     }
 }
