032ff4d0c8e34e9cbc7485c24bc779ab27192869
fde99 <federicofilippo.diana@mail.polimi.it>
2021-04-30 15:14:50 +0200

    removed the big fat switch inside the jsoninterpreterserver class, also refactored the packets

---
+++ src/main/java/it/polimi/ingsw/controller/packets/SetTurnType.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.controller.packets;
+
+import it.polimi.ingsw.controller.ServerController;
+import it.polimi.ingsw.controller.packets.Packet;
+import it.polimi.ingsw.controller.packets.PacketManager;
+
+public class SetTurnType  extends Packet implements PacketManager {
+
+    int turnType;
+
+    public SetTurnType(int turnType)
+    {
+        super("SetTurnType");
+        this.turnType = turnType;
+    }
+    @Override
+    public Packet analyze(ServerController controller) {
+        return null;
+    }
+}
