94b07efe485870c4b80adf0231378310233fd284
NicolaDean <nicola.dean@mail.polimi.it>
2021-05-01 12:03:44 +0200

    Added Packet dedicated to client Controller or server controller

--- src/main/java/it/polimi/ingsw/controller/packets/ACK.java
+++ src/main/java/it/polimi/ingsw/controller/packets/ACK.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.controller.packets;
 
+import it.polimi.ingsw.controller.ClientController;
 import it.polimi.ingsw.controller.ServerController;
 import it.polimi.ingsw.controller.packets.Packet;
 import it.polimi.ingsw.controller.packets.PacketManager;
 
-public class ACK extends Packet<ServerController> implements PacketManager<ServerController>{
+public class ACK extends Packet<ClientController> implements PacketManager<ClientController>{
 
     int errorMSG;
 
@@ -16,7 +17,7 @@ public class ACK extends Packet<ServerController> implements PacketManager<Serve
 
 
     @Override
-    public Packet analyze(ServerController controller)
+    public Packet analyze(ClientController controller)
     {
         //TODO we can have a "error Msg" class with a (array of error) that load from JSON and when i need one i do "getErrorCode( int )"
         //TODO Controller contains the view so i can do "view.printError" or something similar
