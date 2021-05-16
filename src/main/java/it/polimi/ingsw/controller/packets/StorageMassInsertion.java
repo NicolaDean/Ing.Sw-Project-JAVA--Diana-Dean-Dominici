2f3b12bb3d57cb98e80aa60edfe7c5e2773a87fc
NicolaDean <nicola.dean@mail.polimi.it>
2021-05-16 17:30:33 +0200

    Fixed (one file missing from previous commit)

---
+++ src/main/java/it/polimi/ingsw/controller/packets/StorageMassInsertion.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.controller.packets;
+
+import it.polimi.ingsw.controller.ServerController;
+import it.polimi.ingsw.model.resources.Resource;
+import it.polimi.ingsw.model.resources.ResourceList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StorageMassInsertion extends Packet<ServerController> implements PacketManager<ServerController> {
+
+    List<InsertionInstruction> insertions;
+
+    public StorageMassInsertion( List<InsertionInstruction> insertions)
+    {
+        super("StorageMassInsertion");
+        this.insertions = insertions;
+    }
+
+    @Override
+    public Packet analyze(ServerController controller) {
+
+        Packet packet = null;
+        boolean failed = false;
+        List<Resource> remaining = new ResourceList();
+
+        for(InsertionInstruction instruction: insertions)
+        {
+            if(!failed)
+            {
+                packet = instruction.apply(controller,this.getPlayerIndex());
+                if(packet!=null)
+                {
+                    //TODO invert packet(NACK) and Pending gain
+                    failed = true;
+                    remaining.add(instruction.getResource());
+                }
+            }
+            else
+            {
+                remaining.add(instruction.getResource());
+            }
+
+        }
+
+
+        if(failed)
+        {
+            controller.sendMessage(packet,this.getPlayerIndex());
+            return new MarketResult(remaining);
+        }
+        else
+            return new ACK(0);
+
+    }
+}
