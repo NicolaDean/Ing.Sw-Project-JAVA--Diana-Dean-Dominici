64d9b4d6aec0c1a2e3ae6e1aa9fa4ea27156fa62
Riccardo Dominici <36098620+RiccardoDominici@users.noreply.github.com>
2021-05-26 18:05:41 +0200

    Update market GUI

--- src/main/java/it/polimi/ingsw/view/scenes/MarketScene.java
+++ src/main/java/it/polimi/ingsw/view/scenes/MarketScene.java
@@ -7,21 +7,20 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class MarketScene extends BasicSceneUpdater{
 
 
+    private ImageView balls;
+    private ImageView discartedBall;
     @FXML
     AnchorPane pane;
 
     @Override
     public void init() {
         super.init();
-
-        GuiHelper.resize(ConstantValues.guiWidth,ConstantValues.guiHeight);
-        GuiHelper.getStage().show();
-
     }
 
     @Override
